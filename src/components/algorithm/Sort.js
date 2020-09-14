7a8a896e8dc866c17d760167b73eb59052d5a815
Basliel <bababayehu22@wooster.edu>
2020-09-14 14:06:05 -0400

    return a set of animations instead of the modified array

--- src/components/algorithm/Sort.js
+++ src/components/algorithm/Sort.js
@@ -1,18 +1,35 @@
-export const bubbleSort = (array) => {
-  const swap = (array, a, b) => {
+export function getBubbleSortAnimations(array = []) {
+  const swap = (a, b, array) => {
     const temp = array[a];
     array[a] = array[b];
     array[b] = temp;
   };
-  let i = 0;
-  let j = 0;
-  for (i = 0; i < array.length; i++) {
-    for (j = 0; j < array.length - i - 1; j++) {
-      let a = array[j];
-      let b = array[j + 1];
-      if (a > b) {
-        swap(array, j, j + 1);
+  const animations = [];
+  let isSorted = false;
+  let counter = 0;
+  console.log(array);
+  while (!isSorted) {
+    isSorted = true;
+    for (let i = 0; i < array.length - 1 - counter; i++) {
+      if (array[i] > array[i + 1]) {
+        swap(i, i + 1, array);
+        isSorted = false;
+        animations.push([i + 1, i, array[i + 1], array[i]]);
+        animations.push([i + 1, i, array[i + 1], array[i]]);
+        // animations.push([i, i, array[i], array[i]]);
+
+        // animations.push([i + 1, i, array[i + 1], array[i]]);
+
+        animations.push([i + 1, i, array[i + 1], array[i]]);
+      } else {
+        animations.push([i, i + 1, array[i], array[i + 1]]);
+        animations.push([i, i + 1, array[i], array[i + 1]]);
+        animations.push([i, i + 1, array[i], array[i + 1]]);
+        console.log("do nothing");
       }
     }
+    counter++;
   }
-};
+  console.log(array);
+  return animations;
+}
