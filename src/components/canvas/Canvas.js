1888f1641d499d3ec1c21b4cabfa4de5b235866c
Basliel <bababayehu22@wooster.edu>
2020-09-14 14:09:37 -0400

    use animations array to find comparison and swap indicies

--- src/components/canvas/Canvas.js
+++ src/components/canvas/Canvas.js
@@ -1,11 +1,24 @@
 import React, { Component } from "react";
 import "./Canvas.css";
-import * as bubbleSort from "../algorithm/Sort.js";
+import { getBubbleSortAnimations } from "../algorithm/Sort.js";
+
+// Change this value for the speed of the animations.
+const ANIMATION_SPEED_MS = 5;
+
+// Change this value for the number of bars (value) in the array.
+const NUMBER_OF_ARRAY_BARS = 310;
+
+// This is the main color of the array bars.
+const PRIMARY_COLOR = "blueviolet";
+
+// This is the color of array bars that are being compared throughout the animations.
+const SECONDARY_COLOR = "red";
 
 export class Canvas extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      sortIsRunning: false,
       array: [],
     };
   }
@@ -25,30 +38,52 @@ export class Canvas extends Component {
     }
     this.setState({ array });
   };
-  emptyArray = () => {
-    this.setState({ array: [] });
+
+  bubbleSort = () => {
+    const animations = getBubbleSortAnimations(this.state.array);
+    for (let i = 0; i < animations.length; i++) {
+      const arrayBars = document.getElementsByClassName("array-bar");
+      const isColorChange = i % 3 !== 2;
+      // let barOneIdx,
+      //   barTwoIdx,
+      //   barOneHeight,
+      //   barTwoHeight = 0;
+      const [barOneIdx, barTwoIdx, barOneHeight, barTwoHeight] = animations[i];
+
+      if (isColorChange) {
+        const barOneStyle = arrayBars[barOneIdx].style;
+        const barTwoStyle = arrayBars[barTwoIdx].style;
+
+        const color = i % 3 === 0 ? SECONDARY_COLOR : PRIMARY_COLOR;
+        setTimeout(() => {
+          barOneStyle.backgroundColor = color;
+          barTwoStyle.backgroundColor = color;
+        }, i * ANIMATION_SPEED_MS);
+      } else {
+        setTimeout(() => {
+          // const [barOneIdx, barTwoIdx, barOneHeight, barTwoHeight] = animations[i];
+          const barOneStyle = arrayBars[barOneIdx].style;
+          const barTwoStyle = arrayBars[barTwoIdx].style;
+          barOneStyle.width = `${barOneHeight}px`;
+          barTwoStyle.width = `${barTwoHeight}px`;
+        }, i * ANIMATION_SPEED_MS);
+      }
+    }
   };
-
-  sort() {
-    const jsSortedArray = this.state.array.slice().sort();
-    const bubbleSortedArray = bubbleSort(this.state.array);
-
-    console.log(arraysAreEqual(jsSortedArray, bubbleSortedArray));
-  }
 
   render() {
     const { array } = this.state;
-    console.log(array);
     return (
       <div>
         <button onClick={this.createNewArray}>Generate New Array</button>
         {array.map((value, idx) => (
-          <div className="array-bar" key={idx} style={{ width: `${value}px` }}>
-            {" "}
-            {value}
-          </div>
+          <div
+            className="array-bar"
+            key={idx}
+            style={{ width: `${value}px` }}
+          ></div>
         ))}
-        <button onClick={this.sort}> bubbleSort</button>
+        <button onClick={this.bubbleSort}>bubbleSort</button>
       </div>
     );
   }
